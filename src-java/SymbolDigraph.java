38835762f0c8a012e5ba335f289cd4c7b24d352d
Marcelo Cohen <mflashbr@gmail.com>
2022-10-24 20:42:08 -0300

    Classes SymbolGraph/Digraph para representar grafos onde os nodos são Strings

--- src-java/SymbolDigraph.java
+++ src-java/SymbolDigraph.java
@@ -1,3 +1,4 @@
+
 /******************************************************************************
  *  Compilation:  javac SymbolDigraph.java
  *  Execution:    java SymbolDigraph
@@ -44,17 +45,18 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class SymbolDigraph {
-            private static final String NEWLINE = System.getProperty("line.separator");
-    private HashMap<String, Integer> st;  // string -> index
-    private String[] keys;           // index  -> string
-    private Digraph graph;           // the underlying digraph
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private HashMap<String, Integer> st; // string -> index
+    private String[] keys; // index -> string
+    private Digraph graph; // the underlying digraph
 
-    /**  
+    /**
      * Initializes a digraph from a file using the specified delimiter.
      * Each line in the file contains
      * the name of a vertex, followed by a list of the names
      * of the vertices adjacent to that vertex, separated by the delimiter.
-     * @param filename the name of the file
+     * 
+     * @param filename  the name of the file
      * @param delimiter the delimiter between fields
      */
     public SymbolDigraph(String filename, String delimiter) {
@@ -63,13 +65,14 @@ public class SymbolDigraph {
         // First pass builds the index by reading strings to associate
         // distinct strings with an index
         In in = new In(filename);
-        while (in.hasNextLine()) {
+        while (!in.isEmpty()) {
             String[] a = in.readLine().split(delimiter);
             for (int i = 0; i < a.length; i++) {
                 if (!st.containsKey(a[i]))
                     st.put(a[i], st.size());
             }
         }
+        StdOut.println("Done reading " + filename);
 
         // inverted index to get string keys in an aray
         keys = new String[st.size()];
@@ -93,8 +96,10 @@ public class SymbolDigraph {
 
     /**
      * Does the digraph contain the vertex named {@code s}?
+     * 
      * @param s the name of a vertex
-     * @return {@code true} if {@code s} is the name of a vertex, and {@code false} otherwise
+     * @return {@code true} if {@code s} is the name of a vertex, and {@code false}
+     *         otherwise
      */
     public boolean contains(String s) {
         return st.containsKey(s);
@@ -102,8 +107,10 @@ public class SymbolDigraph {
 
     /**
      * Returns the integer associated with the vertex named {@code s}.
+     * 
      * @param s the name of a vertex
-     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
+     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex
+     *         named {@code s}
      * @deprecated Replaced by {@link #indexOf(String)}.
      */
     @Deprecated
@@ -113,8 +120,10 @@ public class SymbolDigraph {
 
     /**
      * Returns the integer associated with the vertex named {@code s}.
+     * 
      * @param s the name of a vertex
-     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex named {@code s}
+     * @return the integer (between 0 and <em>V</em> - 1) associated with the vertex
+     *         named {@code s}
      */
     public int indexOf(String s) {
         return st.get(s);
@@ -122,7 +131,8 @@ public class SymbolDigraph {
 
     /**
      * Returns the name of the vertex associated with the integer {@code v}.
-     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
+     * 
+     * @param v the integer corresponding to a vertex (between 0 and <em>V</em> - 1)
      * @return the name of the vertex associated with the integer {@code v}
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      * @deprecated Replaced by {@link #nameOf(int)}.
@@ -135,7 +145,8 @@ public class SymbolDigraph {
 
     /**
      * Returns the name of the vertex associated with the integer {@code v}.
-     * @param  v the integer corresponding to a vertex (between 0 and <em>V</em> - 1) 
+     * 
+     * @param v the integer corresponding to a vertex (between 0 and <em>V</em> - 1)
      * @return the name of the vertex associated with the integer {@code v}
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
@@ -145,7 +156,8 @@ public class SymbolDigraph {
     }
 
     /**
-     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
+     * Returns the digraph assoicated with the symbol graph. It is the client's
+     * responsibility
      * not to mutate the digraph.
      *
      * @return the digraph associated with the symbol digraph
@@ -157,7 +169,8 @@ public class SymbolDigraph {
     }
 
     /**
-     * Returns the digraph assoicated with the symbol graph. It is the client's responsibility
+     * Returns the digraph assoicated with the symbol graph. It is the client's
+     * responsibility
      * not to mutate the digraph.
      *
      * @return the digraph associated with the symbol digraph
@@ -168,32 +181,33 @@ public class SymbolDigraph {
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
-         int V = graph.V();
+        int V = graph.V();
         if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
     }
 
     public String toDot() {
-		StringBuilder s = new StringBuilder();
-		s.append("graph {"+NEWLINE);
-			s.append("rankdir = LR;"+NEWLINE);
-			s.append("node [shape = circle];"+NEWLINE);
-		for (int v = 0; v < graph.V(); v++)
-		    s.append('"'+name(v)+'"'+";"+NEWLINE);
-    	for (int v = 0; v < graph.V(); v++) {
+        StringBuilder s = new StringBuilder();
+        s.append("graph {" + NEWLINE);
+        s.append("rankdir = LR;" + NEWLINE);
+        s.append("node [shape = circle];" + NEWLINE);
+        for (int v = 0; v < graph.V(); v++)
+            s.append('"' + name(v) + '"' + ";" + NEWLINE);
+        for (int v = 0; v < graph.V(); v++) {
             for (int w : graph.adj(v))
-                s.append('"'+name(v)+'"' + " -> " + '"'+ name(w) + '"' + ";"+NEWLINE);
-            }
+                s.append('"' + name(v) + '"' + " -> " + '"' + name(w) + '"' + ";" + NEWLINE);
+        }
         s.append("}");
         return s.toString();
-        }
+    }
+
     /**
      * Unit tests the {@code SymbolDigraph} data type.
      *
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-        String filename  = args[0];
+        String filename = args[0];
         String delimiter = args[1];
         SymbolDigraph sg = new SymbolDigraph(filename, delimiter);
         Digraph graph = sg.digraph();
@@ -204,11 +218,10 @@ public class SymbolDigraph {
                 for (int v : graph.adj(s)) {
                     StdOut.println("   " + sg.name(v));
                 }
-            }
-            else if(!source.equals("dot")) {
+            } else if (!source.equals("dot")) {
                 StdOut.println("input not contain '" + source + "'");
-            }
-	    else System.out.println(sg.toDot());
+            } else
+                System.out.println(sg.toDot());
         }
     }
 }
